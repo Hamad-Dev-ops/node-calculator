2589946552b4297c92c3b136b4ae285c330c53d6
Hamad Khan <i233095@isb.nu.edu.pk>
2025-10-24 20:07:23 +0500

    updated calculator functionalit

--- calculator.js
+++ calculator.js
@@ -19,10 +19,17 @@ function divide(a, b) {
     return a / b;
 }
 
-// Export functions
+function modulus(a, b) {
+    if (b === 0) {
+        throw new Error("Modulus by zero is not allowed");
+    }
+    return a % b;
+}
+
 module.exports = {
     add,
     subtract,
     multiply,
-    divide
+    divide,
+    modulus
 };
